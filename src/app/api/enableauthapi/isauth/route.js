6ac1025fec4059f4fe69bb2b477f215081774afd
710101414 <73123729+710101414@users.noreply.github.com>
2026-01-29 15:01:44 +0800

    Update route.js

--- src/app/api/enableauthapi/isauth/route.js
+++ src/app/api/enableauthapi/isauth/route.js
@@ -1,30 +1,24 @@
-
 export const runtime = 'edge';
 import { auth } from "@/auth";
 
 export async function GET(request) {
-
   const enableAuthapi = process.env.ENABLE_AUTH_API === 'true';
   const session = await auth();
   const role = session?.user?.role;
-//  console.log(session);
- 
+
   return new Response(
     JSON.stringify({
       status: "success",
-      "message": "You are logged in by user !",
-      "success": true,
-      "enableAuthapi": enableAuthapi,
-      "role": role
+      message: "You are logged in by user !",
+      success: true,
+      enableAuthapi: enableAuthapi,
+      role: role
     }),
     {
       status: 200,
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
     }
-  )
-
+  );
 }
-
-
